62b238125123bd21c06c66d7651ae0ea7a368783
Oh-Su-Jung <tnwjddl1996@gmail.com>
2020-10-06 11:15:22 +0900

    Transaction & Transaction AOP 작업

---
+++ src/test/java/mybatis_spring_study/service/TransactionAOPServiceTest.java
@@ -0,0 +1,88 @@
+package mybatis_spring_study.service;
+
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
+import org.junit.After;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import mybatis_spring_study.dto.Department;
+import mybatis_spring_study.dto.Employee;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/context-root.xml"})
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TransactionAOPServiceTest {
+	private static final Log log = LogFactory.getLog(TransactionAOPServiceTest.class);
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println();
+	}
+
+	@Autowired
+	private TransactionAOPService service;
+
+	
+	@Test(expected = DuplicateKeyException.class)
+	public void testATrRegister_Dept_Fail() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		Department department = new Department(1, "인사", 10); // DuplicateKeyException
+		Employee employee = new Employee(1006, "박규영", "과장", new Employee(4377), 4100000, department);
+		
+		service.trRegister(department, employee);
+	}
+
+	@Test(expected = DuplicateKeyException.class)
+	public void testBTrRegister_Emp_Fail() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		Department department = new Department(7, "인사", 10); // DuplicateKeyException
+		Employee employee = new Employee(4377, "박규영", "과장", new Employee(4377), 4100000, department);
+		
+		service.trRegister(department, employee);
+	}
+
+	@Test
+	public void testCTrRegister_Sucess() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		Department department = new Department(7, "인사", 10); // DuplicateKeyException
+		Employee employee = new Employee(1006, "박규영", "과장", new Employee(4377), 4100000, department);
+		
+		service.trRegister(department, employee);
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void testDTrUnRegister_Dept_Fail() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		Department department = new Department(100); // RuntimeException -> rollback
+		Employee employee = new Employee(1006); // rollback 되므로 삭제되면 안됨
+		
+		service.trUnRegister(department, employee);
+
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void testETrUnRegister_Emp_Fail() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		Department department = new Department(7);// 정상삭제
+		Employee employee = new Employee(9999);// RuntimeException -> rollback
+		
+		service.trUnRegister(department, employee);
+	}
+
+	@Test
+	public void testFTrUnRegister_Sucess() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		Department department = new Department(7);
+		Employee employee = new Employee(1006);
+		
+		service.trUnRegister(department, employee);
+	}
+
+}
